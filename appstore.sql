7f5a119b756ab8072d10897891322193b2ad812a
kakurebono <55621277+kakurebono@users.noreply.github.com>
2020-11-14 16:53:08 +0100

    Oliver

--- appstore.sql
+++ appstore.sql
@@ -71,3 +71,24 @@ ORDER BY rating_count_tot --- it orders based on data type for some reason.
 desc;
 
 
+
+
+--7. Take a look on the data you retrieved in the question 5. Give some insights.
+
+/* top 6 apps are dominationg the market in terms of rating volume and therefore can be assumed that the # downloads are also way higher. 
+ * Chriatianity still dominant 
+ * Facebook is killing it
+ *
+ */
+
+--8. Take a look on the data you retrieved in the question 6. Give some insights.
+
+/* no idea what the actual top 10 APPs are
+
+--9. Now compare the data from questions 5 and 6. What do you see?
+
+--10. How could you take the top 3 regarding the user ratings but also the number of votes?
+
+--11. Does people care about the price? Do some queries, comment why are you doing them and the results you retrieve. What is your conclusion?
+
+

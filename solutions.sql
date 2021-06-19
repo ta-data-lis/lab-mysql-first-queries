03984c7e9bf4c73a2d8e78d4772b6cc51c027cbe
Guilherme Pereira <gr.pereira@campus.fct.unl.pt>
2021-06-19 11:26:20 +0100

    Final Version

--- solutions.sql
+++ solutions.sql
@@ -33,3 +33,33 @@ FROM app.applestore AS app
 GROUP BY app.track_name ORDER BY app.user_rating DESC
 LIMIT 10;
 
+/*7 */
+SELECT prime_genre,rating_count_tot  FROM app.applestore AS app
+GROUP BY rating_count_tot
+ORDER BY rating_count_tot ASC
+
+/*The genre with more rating is Social Networking 
+and the one with none is finance */
+
+/*8 */
+SELECT app.track_name,app.user_rating 
+FROM app.applestore AS app
+GROUP BY app.track_name ORDER BY app.user_rating DESC
+LIMIT 10;
+/*The top user rating is for the app She Works His Way */
+
+/*9 */
+/*The genre Social Networking have an app called She
+works His Way that is in the top 5 of user rating  */ 
+
+/*10*/
+SELECT app.track_name,app.user_rating ,app.rating_count_tot
+FROM app.applestore AS app
+ORDER BY user_rating DESC, app.rating_count_tot DESC
+LIMIT 3;
+
+/*11*/
+SELECT app.track_name,app.user_rating ,app.rating_count_tot,app.price
+FROM app.applestore AS app
+ORDER BY user_rating DESC, app.rating_count_tot DESC, app.price DESC
+LIMIT 100;

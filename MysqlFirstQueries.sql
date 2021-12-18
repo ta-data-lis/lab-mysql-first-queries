b65259298af1a4665f3e8c15735566d1d7d2f28a
beatrizramalho <87273372+beatrizramalho@users.noreply.github.com>
2021-12-18 10:06:31 +0000

    Lab MySQL Firt Queries - Beatriz Ramalho FV

--- MysqlFirstQueries.sql
+++ MysqlFirstQueries.sql
@@ -9,6 +9,7 @@ ORDER BY prime_genre ASC;
 -- 2. Which is the genre with more apps rated?
 SELECT prime_genre, sum(rating_count_tot)
 FROM applestore
+ORDER BY SUM(rating_count_tot) DESC
 LIMIT 1;
 
 -- 3. Which is the genre with more apps?
@@ -40,8 +41,7 @@ LIMIT 10;
 -- 10. How could you take the top 3 regarding the user ratings but also the number of votes?
 SELECT track_name, user_rating, rating_count_tot
 FROM applestore
-WHERE user_rating = 5
-ORDER BY rating_count_tot DESC
+ORDER BY user_rating DESC, rating_count_tot DESC
 LIMIT 3;
 
 -- 11. Does people care about the price?
